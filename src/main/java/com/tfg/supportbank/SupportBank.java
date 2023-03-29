03f896eb22f832303d5fec8219e15c2c5c5040c5
Kara <blk20100@gmail.com>
2023-03-29 17:33:54 +0100

    first commit

---
+++ src/main/java/com/tfg/supportbank/SupportBank.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tfg.supportbank;
+
+
+public class SupportBank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
